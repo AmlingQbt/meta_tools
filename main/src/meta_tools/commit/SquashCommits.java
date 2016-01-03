82383bcaf2683a5bc90f3f97d756fc0e73148e07
Keith Amling <keith.amling@gmail.com>
2016-01-03 15:05:49 -0800

    QbtManifest upgrade framework
    V0 - legacy QbtManifest
    V1 - V0 but with JSON format

--- main/src/meta_tools/commit/SquashCommits.java
+++ main/src/meta_tools/commit/SquashCommits.java
@@ -23,8 +23,11 @@ import qbt.QbtTempDir;
 import qbt.QbtUtils;
 import qbt.VcsVersionDigest;
 import qbt.config.QbtConfig;
-import qbt.manifest.QbtManifest;
-import qbt.manifest.RepoManifest;
+import qbt.manifest.LegacyQbtManifest;
+import qbt.manifest.LegacyQbtManifestBuilder;
+import qbt.manifest.QbtManifestVersions;
+import qbt.manifest.current.QbtManifest;
+import qbt.manifest.current.RepoManifest;
 import qbt.options.ConfigOptionsDelegate;
 import qbt.options.ManifestOptionsDelegate;
 import qbt.options.ManifestOptionsResult;
@@ -66,136 +69,142 @@ public final class SquashCommits extends QbtCommand<SquashCommits.Options> {
     }
 
     @Override
-    public int run(OptionsResults<? extends Options> options) throws Exception {
-        QbtConfig config = Options.config.getConfig(options);
-        ManifestOptionsResult manifestResult = Options.manifest.getResult(options);
-        QbtManifest manifest = manifestResult.parse();
-        Collection<RepoTip> repos = Options.repos.getRepos(config, manifest, options);
+    public int run(final OptionsResults<? extends Options> options) throws Exception {
+        final QbtConfig config = Options.config.getConfig(options);
+        final ManifestOptionsResult manifestResult = Options.manifest.getResult(options);
+        LegacyQbtManifest<?, ?> manifestLegacy = manifestResult.parseLegacy().manifest;
 
-        String metaVcs = options.get(Options.metaVcs);
+        return new Object() {
+            public <M extends LegacyQbtManifest<M, B>, B extends LegacyQbtManifestBuilder<M, B>> int run(LegacyQbtManifest<M, B> manifestLegacy) {
+                QbtManifest manifestCurrent = manifestLegacy.current();
+                Collection<RepoTip> repos = Options.repos.getRepos(config, manifestCurrent, options);
 
-        Path metaDir = QbtUtils.findInMeta("", null);
-        Repository metaRepository = VcsRegistry.getLocalVcs(metaVcs).getRepository(metaDir);
-        if(!metaRepository.isClean()) {
-            throw new IllegalArgumentException("Meta is dirty!");
-        }
-        VcsVersionDigest metaCurrentCommit = metaRepository.getCurrentCommit();
-        CommitData metaCurrentCd = metaRepository.getCommitData(metaCurrentCommit);
+                String metaVcs = options.get(Options.metaVcs);
 
-        ImmutableList.Builder<QbtManifest> parentManifestsBuilder = ImmutableList.builder();
-        for(VcsVersionDigest metaParent : metaCurrentCd.get(CommitData.PARENTS)) {
-            parentManifestsBuilder.add(QbtManifest.parse(metaParent.getRawDigest() + ":qbt-manifest", metaRepository.showFile(metaParent, "qbt-manifest")));
-        }
-        List<QbtManifest> parentManifests = parentManifestsBuilder.build();
-
-        QbtManifest.Builder newManifest = manifest.builder();
-        boolean fail = false;
-        boolean changed = false;
-        try(QbtTempDir tempDir = new QbtTempDir()) {
-            for(RepoTip repo : repos) {
-                RepoManifest repoManifest = manifest.repos.get(repo);
-
-                VcsVersionDigest tip = repoManifest.version;
-                LOGGER.debug("[" + repo + "] tip = " + tip);
-
-                PinnedRepoAccessor pinnedAccessor = config.localPinsRepo.requirePin(repo, tip);
-
-                Path repoDir;
-                Repository repoRepository;
-
-                LocalRepoAccessor localRepoAccessor = config.localRepoFinder.findLocalRepo(repo);
-                if(localRepoAccessor != null) {
-                    repoDir = localRepoAccessor.dir;
-                    repoRepository = localRepoAccessor.vcs.getRepository(localRepoAccessor.dir);
+                Path metaDir = QbtUtils.findInMeta("", null);
+                Repository metaRepository = VcsRegistry.getLocalVcs(metaVcs).getRepository(metaDir);
+                if(!metaRepository.isClean()) {
+                    throw new IllegalArgumentException("Meta is dirty!");
                 }
-                else {
-                    LocalVcs localVcs = pinnedAccessor.getLocalVcs();
-                    repoDir = tempDir.resolve(repo.tip).resolve(repo.name);
-                    QbtUtils.mkdirs(repoDir);
-                    repoRepository = localVcs.createWorkingRepo(repoDir);
+                VcsVersionDigest metaCurrentCommit = metaRepository.getCurrentCommit();
+                CommitData metaCurrentCd = metaRepository.getCommitData(metaCurrentCommit);
+
+                ImmutableList.Builder<QbtManifest> parentManifestsBuilder = ImmutableList.builder();
+                for(VcsVersionDigest metaParent : metaCurrentCd.get(CommitData.PARENTS)) {
+                    parentManifestsBuilder.add(QbtManifestVersions.parse(ImmutableList.copyOf(metaRepository.showFile(metaParent, "qbt-manifest"))));
                 }
+                List<QbtManifest> parentManifests = parentManifestsBuilder.build();
 
-                pinnedAccessor.findCommit(repoDir);
+                B newManifest = manifestLegacy.builder();
+                boolean fail = false;
+                boolean changed = false;
+                try(QbtTempDir tempDir = new QbtTempDir()) {
+                    for(RepoTip repo : repos) {
+                        RepoManifest repoManifest = manifestCurrent.repos.get(repo);
 
-                List<VcsVersionDigest> bases = Lists.newArrayList();
-                for(QbtManifest parentManifest : parentManifests) {
-                    RepoManifest parentRepoManifest = parentManifest.repos.get(repo);
-                    if(parentRepoManifest == null) {
-                        continue;
-                    }
-                    VcsVersionDigest newBase = parentRepoManifest.version;
-                    config.localPinsRepo.requirePin(repo, newBase).findCommit(repoDir);
-                    boolean covered = false;
-                    for(VcsVersionDigest base : bases) {
-                        if(repoRepository.isAncestorOf(newBase, base)) {
-                            covered = true;
-                            break;
+                        VcsVersionDigest tip = repoManifest.version;
+                        LOGGER.debug("[" + repo + "] tip = " + tip);
+
+                        PinnedRepoAccessor pinnedAccessor = config.localPinsRepo.requirePin(repo, tip);
+
+                        Path repoDir;
+                        Repository repoRepository;
+
+                        LocalRepoAccessor localRepoAccessor = config.localRepoFinder.findLocalRepo(repo);
+                        if(localRepoAccessor != null) {
+                            repoDir = localRepoAccessor.dir;
+                            repoRepository = localRepoAccessor.vcs.getRepository(localRepoAccessor.dir);
                         }
-                    }
-                    if(covered) {
-                        continue;
-                    }
-                    bases.add(newBase);
-                }
-                Set<VcsVersionDigest> basesSet = ImmutableSet.copyOf(bases);
+                        else {
+                            LocalVcs localVcs = pinnedAccessor.getLocalVcs();
+                            repoDir = tempDir.resolve(repo.tip).resolve(repo.name);
+                            QbtUtils.mkdirs(repoDir);
+                            repoRepository = localVcs.createWorkingRepo(repoDir);
+                        }
 
-                LOGGER.debug("[" + repo + "] bases = " + bases);
+                        pinnedAccessor.findCommit(repoDir);
 
-                if(basesSet.contains(tip)) {
-                    continue;
-                }
+                        List<VcsVersionDigest> bases = Lists.newArrayList();
+                        for(QbtManifest parentManifest : parentManifests) {
+                            RepoManifest parentRepoManifest = parentManifest.repos.get(repo);
+                            if(parentRepoManifest == null) {
+                                continue;
+                            }
+                            VcsVersionDigest newBase = parentRepoManifest.version;
+                            config.localPinsRepo.requirePin(repo, newBase).findCommit(repoDir);
+                            boolean covered = false;
+                            for(VcsVersionDigest base : bases) {
+                                if(repoRepository.isAncestorOf(newBase, base)) {
+                                    covered = true;
+                                    break;
+                                }
+                            }
+                            if(covered) {
+                                continue;
+                            }
+                            bases.add(newBase);
+                        }
+                        Set<VcsVersionDigest> basesSet = ImmutableSet.copyOf(bases);
 
-                Map<VcsVersionDigest, CommitData> revWalk = repoRepository.revWalk(bases, ImmutableList.of(tip));
-                Iterable<Pair<VcsVersionDigest, CommitData>> revWalkFlat = CommitDataUtils.revWalkFlatten(revWalk, ImmutableList.of(tip));
-                boolean exoticExit = false;
-                for(Pair<VcsVersionDigest, CommitData> p : revWalkFlat) {
-                    VcsVersionDigest commit = p.getLeft();
-                    CommitData cd = p.getRight();
-                    for(VcsVersionDigest parent : cd.get(CommitData.PARENTS)) {
-                        if(revWalk.containsKey(parent)) {
+                        LOGGER.debug("[" + repo + "] bases = " + bases);
+
+                        if(basesSet.contains(tip)) {
                             continue;
                         }
-                        if(basesSet.contains(parent)) {
+
+                        Map<VcsVersionDigest, CommitData> revWalk = repoRepository.revWalk(bases, ImmutableList.of(tip));
+                        Iterable<Pair<VcsVersionDigest, CommitData>> revWalkFlat = CommitDataUtils.revWalkFlatten(revWalk, ImmutableList.of(tip));
+                        boolean exoticExit = false;
+                        for(Pair<VcsVersionDigest, CommitData> p : revWalkFlat) {
+                            VcsVersionDigest commit = p.getLeft();
+                            CommitData cd = p.getRight();
+                            for(VcsVersionDigest parent : cd.get(CommitData.PARENTS)) {
+                                if(revWalk.containsKey(parent)) {
+                                    continue;
+                                }
+                                if(basesSet.contains(parent)) {
+                                    continue;
+                                }
+                                LOGGER.error("[" + repo + "] Exotic exit from bases..tip via " + commit.getRawDigest() + " -> " + parent.getRawDigest());
+                                exoticExit = true;
+                                break;
+                            }
+                            if(exoticExit) {
+                                break;
+                            }
+                        }
+                        if(exoticExit) {
+                            fail = true;
                             continue;
                         }
-                        LOGGER.error("[" + repo + "] Exotic exit from bases..tip via " + commit.getRawDigest() + " -> " + parent.getRawDigest());
-                        exoticExit = true;
-                        break;
+
+                        CommitData newCd = revWalk.get(tip);
+                        newCd = newCd.set(CommitData.MESSAGE, metaCurrentCd.get(CommitData.MESSAGE));
+                        newCd = newCd.set(CommitData.PARENTS, ImmutableList.copyOf(bases));
+                        VcsVersionDigest newTip = repoRepository.createCommit(newCd);
+
+                        if(newTip.equals(tip)) {
+                            continue;
+                        }
+
+                        changed = true;
+                        pinnedAccessor.addPin(repoDir, newTip);
+                        newManifest = newManifest.withRepoVersion(repo, newTip);
+                        LOGGER.info("[" + repo + "] Rebuilt " + revWalk.size() + " commit(s) behind " + tip.getRawDigest() + " -> " + newTip.getRawDigest());
                     }
-                    if(exoticExit) {
-                        break;
-                    }
                 }
-                if(exoticExit) {
-                    fail = true;
-                    continue;
+                if(fail) {
+                    return 1;
                 }
-
-                CommitData newCd = revWalk.get(tip);
-                newCd = newCd.set(CommitData.MESSAGE, metaCurrentCd.get(CommitData.MESSAGE));
-                newCd = newCd.set(CommitData.PARENTS, ImmutableList.copyOf(bases));
-                VcsVersionDigest newTip = repoRepository.createCommit(newCd);
-
-                if(newTip.equals(tip)) {
-                    continue;
+                if(!changed) {
+                    LOGGER.info("No change.");
+                    return 0;
                 }
-
-                changed = true;
-                pinnedAccessor.addPin(repoDir, newTip);
-                newManifest = newManifest.with(repo, repoManifest.builder().set(RepoManifest.VERSION, newTip));
-                LOGGER.info("[" + repo + "] Rebuilt " + revWalk.size() + " commit(s) behind " + tip.getRawDigest() + " -> " + newTip.getRawDigest());
+                manifestResult.deparse(newManifest.build());
+                VcsVersionDigest commit = metaRepository.commit(true, metaCurrentCd.get(CommitData.MESSAGE), CommitLevel.MODIFIED);
+                LOGGER.info("[meta] Committed (amend) " + commit.getRawDigest());
+                return 0;
             }
-        }
-        if(fail) {
-            return 1;
-        }
-        if(!changed) {
-            LOGGER.info("No change.");
-            return 0;
-        }
-        manifestResult.deparse(newManifest.build());
-        VcsVersionDigest commit = metaRepository.commit(true, metaCurrentCd.get(CommitData.MESSAGE), CommitLevel.MODIFIED);
-        LOGGER.info("[meta] Committed (amend) " + commit.getRawDigest());
-        return 0;
+        }.run(manifestLegacy);
     }
 }
